a1afca11b5c1f85fedb40d12fdb46d52fb285861
xaviachristi <xaviachristi@gmail.com>
2025-07-23 14:14:37 +0100

    Update

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+
 let firstCard = 7
 let secondCard = 9
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard
-console.log(sum)
+
+if (sum<21) {
+    console.log("Do you want to draw a new card?")
+}
+else if (sum === 21) {
+    console.log("You've got Blackjack! 🥳")
+}
+else {
+    console.log("You're out of the game! 😭")
+}
--- notes.md
+++ notes.md
@@ -3,3 +3,4 @@
 - Aim of Game: Get a hand that sums up to 21 exactly.
     - If not, as close to 21 as you can, but never >21.
     - If >21, out of game
+    - King = 10, Ace = 11
