598691359d72d842dee5fe18bc1ec2cb6c0bb491
xaviachristi <xaviachristi@gmail.com>
2025-07-24 23:21:05 +0100

    Complete game

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 
-let firstCard = 7
-let secondCard = 9
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+    name: "Eva",
+    chips: 137
+}
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 // let sumEl = document.querySelector("#sum-el") // # = element w the id of "sum-el". asking for an element by its selector.
 let cardsEl = document.getElementById("cards-el")
 
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() { //function declarations get hoisted to the top = accessible even at line 1, no matter where it's written within the code
+    let randomNumber = Math.floor( Math.random() * 13 ) + 1 //generates random number between 1 and 13
+    if (randomNumber===1) {
+        randomNumber=11
+    }
+    else if (randomNumber>10) {
+        randomNumber=10
+    }
+    return randomNumber
+}
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    
     sumEl.textContent = "Sum: " + sum    
-
     if (sum<=20) {
         message = "Do you want to draw a new card?"
     }
@@ -35,8 +60,11 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 8
-    sum += card
-    cards.push(card)
-    renderGame()
+    if (isAlive && hasBlackjack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
+
