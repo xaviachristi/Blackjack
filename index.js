81e64bc053929b6e6e07b2f6f4158aa435da20b6
xaviachristi <xaviachristi@gmail.com>
2025-07-24 12:49:29 +0100

    Styling:

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ let hasBlackjack = false
 let isAlive = true
 let message = ""
 
-if (sum<=20) {
-    message = "Do you want to draw a new card?"
-}
-else if (sum===21) {
-    message = "You've got Blackjack! 🥳"
-    hasBlackjack = true
-}
-else {
-    message = "You're out of the game! 😭"
-    isAlive = false
-}
 
-console.log(message)
+function startGame() {
+    if (sum<=20) {
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum===21) {
+        message = "You've got Blackjack! 🥳"
+        hasBlackjack = true
+    }
+    else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+
+    console.log(message)
+    }
